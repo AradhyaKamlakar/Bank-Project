12b0a1c9f110827f1b57082ed7a0f4669fe5d063
GeeksforGeeks <kalegauree44@gmail.com>
2023-04-03 12:49:08 +0530

    Login Logic Added

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,7 +1,40 @@
-﻿namespace Bank.Controllers
+﻿using Bank.Interfaces;
+using Bank.Model;
+using Bank.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Controllers
 {
-    public class LoginController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
     {
+        public readonly IUser _iuser;
+        public LoginController(IUser iuser)
+        {
+            _iuser = iuser;
+        }
+
+        [HttpPost]
+       public IActionResult Login(User u)
+        {
+            var existingUser = _iuser.GetUsers();
+
+            if(existingUser == null)
+            {
+                return NotFound();
+            }
+
+            foreach(var user in existingUser)
+            {
+                if(user.Name == u.Name && user.Password == u.Password ) 
+                {
+                    return Ok(user.Id);
+                }
+            }
+            return NotFound();
+        }
+
 
     }
 }
